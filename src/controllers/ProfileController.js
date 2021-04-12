3fa4cc5045602156a15c69e744b0c74e86a91f99
Henrique <rebeloehenrique@hotmail.com>
2021-04-12 00:40:01 -0300

    First Commit

---
+++ src/controllers/ProfileController.js
@@ -0,0 +1,29 @@
+const Profile = require("../model/Profile");
+
+module.exports = {
+  async index(req, res) {
+    res.render("profile", { profile: await Profile.get() });
+  },
+  async update(req, res) {
+    const data = req.body;
+
+    const weekPerYear = 52;
+
+    const weeksPerMonth = (weekPerYear - data["vacation-per-year"]) / 12;
+
+    const weekTotalHours = data["hours-per-day"] * data["days-per-week"];
+
+    const monthlyTotalHours = weekTotalHours * weeksPerMonth;
+
+    const valueHour = (data["value-hour"] =
+      data["monthly-budget"] / monthlyTotalHours);
+
+    await Profile.update({
+      ...await Profile.get(),
+      ...req.body,
+      "value-hour": valueHour,
+    });
+
+    return res.redirect("/profile");
+  },
+};
